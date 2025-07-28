a19ece8732746d63320e652f81b3877934b08a96
Eda Öney <oneeyyeda@gmail.com>
2025-07-28 18:35:12 +0300

    Completed

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("KULLANICI DOĞRULAMA");
+
+Console.Write("Ad Soyad: ");
+string calısan  = Console.ReadLine();
+
+Console.Write("Şifre giriniz: ");
+string password = Console.ReadLine();
+
+string dogrusifre = "Malatya44";
+
+if (dogrusifre == password)
+{
+    Console.WriteLine("Hoşgeldin Yönetici :) ");
+}
+
+if (dogrusifre != password)
+{
+    Console.WriteLine("Hatalı giriş");
+}
